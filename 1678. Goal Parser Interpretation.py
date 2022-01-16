e79ca6151ddfcb146a7266fa1818efe3be2e0e1d
anna-osipchyk <annaosipchik03@gmail.com>
2022-01-16 20:19:09 +0300

    1678 better

--- 1678. Goal Parser Interpretation.py
+++ 1678. Goal Parser Interpretation.py
@@ -1,15 +1,9 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        dictionary = {
-            "()": "o",
-            "(al)": "al"
-        }
-        for key, value in dictionary.items():
-            command = command.replace(key, value)
-        return command
+        return command.replace("()", "o").replace("(al)", "al")
 
 
 ret = Solution().interpret("G()(al)")
 
-# Runtime: 39 ms, faster than 28.90% of Python3 online submissions for Goal Parser Interpretation.
+# Runtime: 28 ms, faster than 85.08% of Python3 online submissions for Goal Parser Interpretation.
 # Memory Usage: 14.1 MB, less than 89.69% of Python3 online submissions for Goal Parser Interpretation.
